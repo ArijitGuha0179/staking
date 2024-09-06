fa700b5752939de836f42c3a0eb0a69df31b1be5
ArijitGuha0179 <archi4arijit@gmail.com>
2024-09-06 20:00:14 +0530

    token ICO contract integrated in index js

--- Context/index.js
+++ Context/index.js
@@ -157,3 +157,273 @@ export async function deposit(poolID,amount,address){
         notifyError(errorMsg);
     }
 }
+
+export async function transferToken(amount,transferAddress){
+    try{
+        notifySuccess("calling Contract Token...");
+        const stakingTokenObj=await tokenContract();
+
+        const transferAmount=ethers.utils.parseEther(amount);
+        const approveTx=await stakingTokenObj.transfer(transferAddress,transferAmount);
+        await approveTx.wait();
+        notifySuccess("Token transfered successfully");
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export async function withdraw(poolID,amount){
+    try{
+       notifySuccess("Calling Contract...");
+       
+        const amountInWei=ethers.utils.parseUnits(amount.toString(),18);
+        const contractObj=await contract();
+        const gasEstimation=await contractObj.estimateGas.withdraw(Number(poolID),amountInWei);
+
+        const data= await contractObj.withdraw(Number(poolID),amountInWei,{gasLimit:gasEstimation});
+
+        const receipt=await data.wait();
+        notifySuccess("Token withdrawn successfully");
+        return receipt;
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export async function claimReward(poolID){
+    try{
+        notifySuccess("Calling Contract...");
+        const contractObj=await contract();
+        const gasEstimation=await contractObj.estimateGas.claimReward(Number(poolID));
+
+        const data= await contractObj.claimReward(Number(poolID),{gasLimit:gasEstimation});
+        const receipt=await data.wait();
+        notifySuccess("Reward claimed successfully");
+        return receipt;
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export async function createPool(pool){
+    try{
+        const {_depositToken,_rewardToken,_apy,_lockDays}=pool
+        if(!_depositToken || !_rewardToken || !_apy || !_lockDays){
+            
+            return notifyError("Please fill all fields");
+        }
+        notifySuccess("Calling Contract...");
+        const contractObj=await contract();
+        
+        const gasEstimation=await contractObj.estimateGas.addPool(_depositToken,_rewardToken,Number(_apy),Number(_lockDays));
+
+        const stakeTx= await contractObj.addPool(_depositToken,_rewardToken,Number(_apy),Number(_lockDays),{gasLimit:gasEstimation});
+
+        const receipt=await stakeTx.wait();
+        notifySuccess("Pool created successfully");
+        return receipt;
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export async function modifyPool(poolID,amount){
+    try{
+        notifySuccess("Calling Contract...");
+        const contractObj=await contract();
+        
+        const gasEstimation=await contractObj.estimateGas.modifyPool(Number(poolID),Number(amount));
+
+        const data= await contractObj.modifyPool(Number(poolID),Number(amount),{gasLimit:gasEstimation});
+
+        const receipt=await data.wait();
+        notifySuccess("Pool modified successfully");
+        return receipt;
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export async function sweep(tokenData){
+    try{
+        const {token,amount}=tokenData;
+        if(!token || !amount){
+            return notifyError("Please fill all fields");
+        }
+        notifySuccess("Calling Contract...");
+        const contractObj=await contract();
+        const transferAmount=ethers.utils.parseEther(amount);
+        const gasEstimation=await contractObj.estimateGas.sweep(token,transferAmount);
+
+        const data= await contractObj.sweep(token,transferAmount,{gasLimit:gasEstimation});
+        const receipt=await data.wait();
+        notifySuccess("Token Swept successfully");
+        return receipt;
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+// ADD token in metamask
+
+export const addTokenMetaMask=async(token)=>{
+    if(window.ethereum){
+        const contract=await tokenContract();
+        const tokenDecimal=await contract.decimals();
+        const tokenAddress=await contract.address;
+        const tokenSymbol=await contract.symbol();
+        const tokenImage=await TOKEN_LOGO;
+        try{
+            const wasAdded=await window.ethereum.request({
+                method:"wallet_watchAsset",
+                params:{
+                    type:"ERC20",
+                    options:{
+                        address:tokenAddress,
+                        symbol:tokenSymbol,
+                        decimals:tokenDecimal,
+                        image:tokenImage,
+                    },
+                },
+            });
+            if(wasAdded){
+                notifySuccess("Token added in metamask");
+            }
+            else{
+                notifyError("Failed to add in metamask");
+            }
+        }catch(error){
+            console.log(error);
+            notifyError("Failed to add in metamask");
+        }
+    }else{
+        notifyError("Metamask not installed");
+    }
+};
+
+//ICO contract functionality
+
+export const BUY_TOKEN=async(amount)=>{
+    try{
+        notifySuccess("Calling ICO Contract...");
+        const contract=await TOKEN_ICO_CONTRACT();
+
+        const tokenDetails=await contract.gettokenDetails();
+
+        const availableToken=ethers.utils.parseEther(tokenDetails.balance.toString());
+
+        if(availableToken>1){
+            const price=ethers.utils.formatEther(tokenDetails.price.toString()) * Number(amount);
+
+            const payAmount=ethers.utils.parseEther(price.toString(),"ether");
+
+            const transaction= await contract.buyToken(Number(amount),{
+                value:payAmount.toString(),
+                gasLimit: ethers.utils.hexlify(8000000)
+            });
+            const receipt=await transaction.wait();
+
+            notifySuccess("Token bought successfully");
+            return receipt;
+        }else{
+            notifyError("Token Balance is low");
+            return "receipt";
+        }
+
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export const BUY_WITHDRAW=async()=>{
+    try{    
+        notifySuccess("Calling ICO Contract...");
+        const contract=await TOKEN_ICO_CONTRACT();
+        const tokenDetails=await contract.gettokenDetails();
+
+        const availableToken=ethers.utils.parseEther(tokenDetails.balance.toString());
+        if(availableToken>1){
+            const transaction= await contract.withdrawTokens();
+            const receipt=await transaction.wait();
+
+            notifySuccess("Token withdraw successfully done");
+            return receipt;
+        }else{
+            notifyError("Token Balance is low");
+            return "receipt";
+        }
+
+        
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export const UPDATE_TOKEN=async(_address)=>{
+    try{
+        if(!_address){
+            return notifyError("Please fill address");
+        };
+
+        const contract=await TOKEN_ICO_CONTRACT();
+
+        const gasEstimation=await contract.estimateGas.updateToken(_address);
+
+        const transaction=await contract.updateToken(_address,{gasLimit:gasEstimation});
+
+        const receipt=await transaction.wait();
+
+        notifySuccess("Token updated successfully");
+        return receipt;
+        
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
+
+export const UPDATE_TOKEN_PRICE=async(price)=>{
+    try{
+        if(!price){
+            return notifyError("Please fill price");
+        };
+        const contract=await TOKEN_ICO_CONTRACT();
+        const payAmount=ethers.utils.parseEther(price.toString(),"ether");
+        const gasEstimation=await contract.estimateGas.updateTokenSalePrice(payAmount);
+
+        const transaction=await contract.updateTokenSalePrice(payAmount,{gasLimit:gasEstimation});
+
+        const receipt=await transaction.wait();
+
+        notifySuccess("Token price updated successfully");
+        return receipt;
+        
+    }catch(error){
+        console.log(error);
+        const errorMsg=parseErrorMsg(error);
+        notifyError(errorMsg);
+    }
+}
