14f2d3d623e99d138a11747967233c87bda92d1b
ArijitGuha0179 <archi4arijit@gmail.com>
2024-09-07 21:14:31 +0530

    admin panel created

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,17 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import { useAccount } from "wagmi";
+
+
+import {Header,Footer,Loader,ICOSale} from "../Components/index";
+import Admin from "../Components/Admin/Admin";
+import AdminHead from "../Components/Admin/AdminHead";
+import UpdateAPYModel from "../Components/Admin/UpdateAPYModel";
+import Auth from "../Components/Admin/Auth";
+
+
+import {CONTRACT_DATA,transferToken,createPool,sweep,modifyPool} from "../Context/index";
+
+const ADMIN_ADDRESS= process.env.NEXT_PUBLIC_ADMIN_ADDRESS;
 
 const admin = () => {
   return <div>admin</div>;
