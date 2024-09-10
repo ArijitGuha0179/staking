7054ce5aa3c2a08d82282a85289718f58e5952f8
ArijitGuha0179 <archi4arijit@gmail.com>
2024-09-10 17:50:14 +0530

    Admin Panel

--- Components/Admin/Auth.jsx
+++ Components/Admin/Auth.jsx
@@ -4,12 +4,14 @@ const Auth = () => {
   return (<div className="new-loader-wrapper-admin">
     <div className="modal--auto">
       <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
         <div className="modal__content">
           <h4 className="modal__title">Admin Login</h4>
           <p className="modal__text">Your are not the Admin buddy</p>
           <div className="modal__form">
             <a href="/" className="form__btn" type="button">Go to Home</a>
           </div>
+        </div>
         </div>
       </div>
     </div>
