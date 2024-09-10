6297c60e882134d42fb83cc272f60523a2d2b641
ArijitGuha0179 <archi4arijit@gmail.com>
2024-09-10 21:21:01 +0530

    Ask,Partners and Herosection added

--- Components/Partners.jsx
+++ Components/Partners.jsx
@@ -43,7 +43,36 @@ const Partners = () => {
       url: "https://www.theblockchaincoders.com/pro-nft-marketplace",
     },
   ];
-  return <div></div>;
+  return (
+    <section id="partners" className="section">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-8 offset-xl-2">
+            <div className="section__title">
+              <h2>Our Partners</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptates, doloremque.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {partners.map((partner, index) => (
+            <div className="col-6 col-lg-3" key={index}>
+              <a href={partner.url} className="partner">
+                <img src={partner.image} alt="" />
+                <p>{partner.name}</p>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+
+
+    </section>
+
+  );
 };
 
 export default Partners;
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,76 @@ import {Header,HeroSection,Footer,Pools,PoolsModel,WithdrawModal,Withdraw,Partne
 import {CONTRACT_DATA,deposit,withdraw,claimReward,addTokenMetaMask} from '../Context/index';
 
 const index = () => {
+  const {address}=useAccount();
+  const [loader,setLoader]=useState(false);
+  const [contactUs,setContactUs]=useState(false);
+  const [poolID,setPoolID]=useState();
+  const [withdrawPoolID,setWithdrawPoolID]=useState();
+  const [poolDetails,setPoolDetails]=useState();
+  const [selectedPool,setSelectedPool]=useState();
+  const [selectedToken,setSelectedToken]=useState();
+
+  const LOAD_DATA=async()=>{
+    if(address){
+      setLoader(true);
+      const data=await CONTRACT_DATA(address);
+      console.log("Data Loaded",data);
+      setPoolDetails(data);
+      setLoader(false);
+    }
+  }
+  useEffect(()=>{
+    LOAD_DATA();
+  },[address]);
   return (
     <>
       <Header page={"admin"}/>
+      <HeroSection
+       poolDetails={poolDetails}
+       addTokenMetaMask={addTokenMetaMask}
+      />
+      <Statistics poolDetails={poolDetails}/>
+      <Pools 
+       setPoolID={setPoolID}
+       poolDetails={poolDetails}
+       selectedPool={selectedPool}
+       selectedToken={selectedToken}
+      />
+      <Token
+       poolDetails={poolDetails}
+       
+      />
+
+      <Withdraw
+       setWithdrawPoolID={setWithdrawPoolID}
+       poolDetails={poolDetails}
+      />
+
+      <Notification
+      poolDetails={poolDetails}
+      />
+      <Partners/>
+      <Ask setContactUs={setContactUs}/>
       <Footer/>
+
+      {/* Modals */}
       
+      <PoolsModel
+      deposit={deposit}
+      poolID={poolID}
+      address={address}
+      selectedPool={selectedPool}
+      selectedToken={selectedToken}
+      setLoader={setLoader}
+      />
+      <WithdrawModal
+      withdraw={withdraw}
+      withdrawPoolID={withdrawPoolID}
+      address={address}
+      setLoader={setLoader}
+      claimReward={claimReward}
+      />
+      <ICOSale setLoader={setLoader}/>
     </>
   ); 
   
