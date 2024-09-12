69aa4c93633a507295b3d444571a9d482da061a1
ArijitGuha0179 <archi4arijit@gmail.com>
2024-09-12 10:32:06 +0530

    activities page created

--- pages/activities.js
+++ pages/activities.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import { useAccount } from "wagmi";
+
+
+import {Header,Loader,ICOSale,Statistics,Notification} from "../Components/index";
+
+import {CONTRACT_DATA} from "../Context/index";
 
 const activities = () => {
-  return <div>activities</div>;
+  const {address} =useAccount();
+  const [loader,setLoader]=useState(false);
+  const [poolDetails,setPoolDetails]=useState();
+  const LOAD_DATA=async()=>{
+    if(address){
+      setLoader(true);
+      
+      
+        const data=await CONTRACT_DATA(address);
+        
+        setPoolDetails(data);
+      
+      setLoader(false);  
+    }
+  };
+  useEffect(()=>{
+    LOAD_DATA();
+  },[address]);
+  return (
+  <>
+    <Header page={"activity"}/>
+    <div className="new-margin">
+      <h1>This is the Activities Page</h1>
+    </div>
+      <Statistics poolDetails={poolDetails} />
+      <Notification page={"activity"} poolDetails={poolDetails} />
+      <ICOSale setLoader={setLoader} />
+      {loader && <Loader />}
+    
+  </>
+  );
 };
 
 export default activities;
